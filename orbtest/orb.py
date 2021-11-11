09c3594d84cd27bb6e228c4489134d425ba4c62d
Blake Bourque <techplex.engineer@gmail.com>
2021-11-11 11:20:19 -0500

    Add algorithm variations

--- orbtest/orb.py
+++ orbtest/orb.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2
+import cv2.cv2 as cv2
 
 
 # Read the query image as query_img
@@ -7,38 +7,71 @@ import cv2
 # is what you need to find in train image
 # Save it in the same directory
 # with the name image.jpg
-query_img = cv2.imread('score_overlay_2020_1280.png')
-train_img = cv2.imread('2020/frame0011-auto.jpg')
+template = cv2.imread('score_overlay_2021_1280.png')
+target = cv2.imread('2021/frame-00570.jpg')
 
 # Convert it to grayscale
-query_img_bw = cv2.cvtColor(query_img, cv2.COLOR_BGR2GRAY)
-train_img_bw = cv2.cvtColor(train_img, cv2.COLOR_BGR2GRAY)
+# template_bw = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+# target_bw = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
 
 # Initialize the ORB detector algorithm
-orb = cv2.ORB_create()
+algo = cv2.ORB_create()
 
 # Now detect the keypoints and compute
 # the descriptors for the query image
 # and train image
-queryKeypoints, queryDescriptors = orb.detectAndCompute(query_img_bw, None)
-trainKeypoints, trainDescriptors = orb.detectAndCompute(train_img_bw, None)
+template_kp, template_des = algo.detectAndCompute(template, None)
+target_kp, target_des = algo.detectAndCompute(target, None)
 
 # Initialize the Matcher for matching
 # the keypoints and then match the
 # keypoints
 matcher = cv2.BFMatcher()
-matches = matcher.match(queryDescriptors, trainDescriptors)
+matches = matcher.match(template_des, target_des)
+# matches = matcher.knnMatch(template_des, target_des, k=2)
+
+# Apply ratio test
+# good = []
+# for m,n in matches:
+#     if m.distance < 0.75*n.distance:
+#         good.append([m])
+# cv.drawMatchesKnn expects list of lists as matches.
+# final_img = cv2.drawMatchesKnn(template,template_kp,target,target_kp,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+# Show the final image
+# cv2.imshow("Matches", final_img)
+# cv2.waitKey()
+
+# FLANN_INDEX_KDTREE = 0
+# index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+# search_params = dict(checks=50)
+# matcher = cv2.FlannBasedMatcher(index_params, search_params)
+# matches = matcher.knnMatch(template_kp, target_kp, k=2)
+
+# # Apply ratio test
+# good = []
+# for m,n in matches:
+#     if m.distance < 0.75*n.distance:
+#         good.append([m])
+#
+# # cv.drawMatchesKnn expects list of lists as matches.
+# final_img = cv2.drawMatchesKnn(template, template_kp,
+#                                target, target_kp, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+#
+# # Show the final image
+# cv2.imshow("Matches", final_img)
+# cv2.waitKey()
 
 # draw the matches to the final image
 # containing both the images the drawMatches()
 # function takes both images and keypoints
 # and outputs the matched query image with
 # its train image
-final_img = cv2.drawMatches(query_img, queryKeypoints,
-                            train_img, trainKeypoints, matches[:20], None)
-
-final_img = cv2.resize(final_img, (1000, 650))
-
+final_img = cv2.drawMatches(template, template_kp,
+                            target, target_kp, matches[:20], None)
+#
+# final_img = cv2.resize(final_img, (1000, 650))
+#
 # Show the final image
 cv2.imshow("Matches", final_img)
 cv2.waitKey()
---
+++ orbtest/sift.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2.cv2 as cv
+
+img1 = cv.imread('score_overlay_2021_1280.png', cv.IMREAD_GRAYSCALE)  # queryImage
+img2 = cv.imread('2021/frame-00570.jpg', cv.IMREAD_GRAYSCALE)  # trainImage
+
+# Initiate SIFT detector
+sift = cv.SIFT_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1, None)
+kp2, des2 = sift.detectAndCompute(img2, None)
+
+# BFMatcher with default params
+bf = cv.BFMatcher()
+matches = bf.knnMatch(des1, des2, k=2)
+
+# Apply ratio test
+good = []
+for m, n in matches:
+    if m.distance < 0.75 * n.distance:
+        good.append(m)
+
+src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+t = cv.estimateAffinePartial2D(src_pts, dst_pts)
+
+transform = {
+    'scale': t[0][0, 0],
+    'tx': t[0][0, 2],
+    'ty': t[0][1, 2],
+}
+TEMPLATE_SCALE = 1
+
+
+def transformPoint(point):
+    # Transforms a point from template coordinates to image coordinates
+    scale = transform['scale'] * TEMPLATE_SCALE
+    tx = transform['tx']
+    ty = transform['ty']
+    return np.int32(np.round(point[0] * scale + tx)), np.int32(np.round(point[1] * scale + ty))
+
+
+def _cornersToBox(tl, br):
+    return np.array([
+        [tl[0], tl[1]],
+        [br[0], tl[1]],
+        [br[0], br[1]],
+        [tl[0], br[1]]
+    ])
+
+
+def _drawBox(img, box, color):
+    cv.polylines(img, [box], True, color, 2, cv.LINE_AA)
+
+
+box = _cornersToBox(transformPoint((0, 0)), transformPoint((1280, 170)))
+_drawBox(img2, box, (255, 255, 0))
+
+img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, [[m] for m in good], None,
+                         flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+cv.imshow("Matches", img3)
+cv.waitKey()
