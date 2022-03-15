77610dc56957256373333e9726aef81521408f71
Blake Bourque <Techplex.Engineer@gmail.com>
2022-03-15 09:29:54 -0400

    Add dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,16 @@ setup(
     url='https://github.com/TechplexEngineer/frc-livescore',
     keywords=['frc', 'score', 'robotics'],
     license='MIT',
+    package_dir={"": "livescore"},
     packages=find_packages(exclude=('tests', 'docs')),
-    package_data={'livescore': ['templates/*.png', 'tessdata/*.traineddata', 'training_data/*.pkl']}
+    package_data={'livescore': ['templates/*.png', 'tessdata/*.traineddata', 'training_data/*.pkl']},
+    install_requires=[
+        'pytesseract==0.3.9',
+        'numpy>=1.14.0', #1.22.3
+        'regex==2022.3.2',
+        'pyyaml==6.0',
+        'opencv-python==4.5.5.64',
+        'opencv-contrib-python==4.5.5.64',
+        'Pillow==9.0.1',
+    ]
 )
